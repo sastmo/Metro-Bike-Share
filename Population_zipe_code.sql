1d443f3e4b8ce5b2edc87f4312b81700848fadde
sastmo <116411251+sastmo@users.noreply.github.com>
2023-02-15 19:01:03 -0500

    Update Population_zipe_code.sql

--- Population_zipe_code.sql
+++ Population_zipe_code.sql
@@ -24,5 +24,26 @@ CREATE OR REPLACE VIEW Population_by_zip_code AS
  FROM    Population_zip_code AS p
 
 
+DROP TABLE IF EXISTS zip_code_location;
+CREATE TABLE zip_code_location (
+  zip_code      int PRIMARY KEY,
+  latitude      decimal,
+  longitude     decimal
+  );
+
+\copy zip_code_location FROM '/Users/admin/OneDrive/Desktop/Dataset/Us_zip_codes.csv' WITH (FORMAT csv, HEADER TRUE);
+
+TABLE zip_code_location;
+
+
+DROP TABLE IF EXISTS zip_code_land;
+CREATE TABLE zip_code_land (
+  ZIP_code           int,
+  Land_area_Sqm    decimal
+  );
+
+\copy zip_code_land FROM '/Users/admin/OneDrive/Desktop/Dataset/Zipcode_land_area.csv' WITH (FORMAT csv, HEADER TRUE);
+
+TABLE zip_code_land;
 
 
